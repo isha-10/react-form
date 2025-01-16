bdb2030f6a74469ba7aee567b6419c53d1236153
isha khan <165405250+isha-10@users.noreply.github.com>
2025-01-16 02:46:13 -0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+"use client";
+import Form from "./components/Form";
+
+function Home() {
+
+  return (
+    <main className=" bg-slate-500 flex min-h-screen flex-col items-center justify-between p-24">
+     <Form />
+    </main>
+  );
+}
+
+export default Home;
